ae24632fa051007f727ce66c07527083fd1f97f8
Sean Pierson <mrseanpierson1@gmail.com>
2015-11-08 15:42:51 -0800

    edited index.html to use a different drop down so that getting the data from it will be easier. Put all of the data manipulation code that I wrote into a helper funciton called getData() in viz.js

--- public/viz.js
+++ public/viz.js
@@ -34,7 +34,7 @@ var majorSet = {
 
 	
 
-var selectedMajor = majorSet.business;
+var selectedMajor;
 var chart;
 var options = {
         width: 700,
@@ -44,7 +44,7 @@ var options = {
             title: 'Research Start'
         },
         vAxis: {
-            title: 'Confidence'
+            title: 'Average Confidence'
         },
 		legend: { 
 	    	position: 'none' 
@@ -55,106 +55,110 @@ var options = {
         }
     };
     
-function vizController(selectedMajor) {
+function vizController(buttonMajor) {
 
-	if(views[selectedMajor] === undefined){
-		views[selectedMajor] = new google.visualization.DataView(ourData);
-		views[selectedMajor].setRows(views[selectedMajor].getFilteredRows([{column: 0, value: selectedMajor}]));
-		views[selectedMajor].setColumns([1,3]);
-		chart.draw(views[selectedMajor].toDataTable(), options);
-	}
+	selectedMajor = parseInt(buttonMajor);
+	getData();
 }
 
-function vizInit() {
+function getData() {
+	chart = new google.visualization.ColumnChart(document.getElementById('ex0'));
+	chart.groupWidth = 100;
+	//this gets the entire table
+	var query = "SELECT major, start, confidence FROM 1fxvCbqTZgT21sArvYIp6zXBQzgCmVNUSwBZtu-BX"
+	var opts = {sendMethod: 'auto'};
+	var queryObj = new google.visualization.Query('https://www.google.com/fusiontables/gvizdata?tq=', opts);
+		//#rows:id=1
 
-// Create a new viz object using the google API -- specifically,                                                                                                         
-// in the html file
-chart = new google.visualization.ColumnChart(document.getElementById('ex0'));
-chart.groupWidth = 100;
-//this gets the entire table
-var query = "SELECT major, start, confidence FROM 1fxvCbqTZgT21sArvYIp6zXBQzgCmVNUSwBZtu-BX"
-var opts = {sendMethod: 'auto'};
-var queryObj = new google.visualization.Query('https://www.google.com/fusiontables/gvizdata?tq=', opts);
-	//#rows:id=1
+		// Send the query and handle the response by logging the data
+		// to the console.
+		queryObj.setQuery(query);                                                                
+		queryObj.send(function(e) {
+	    
+		var countEncylo = 0;
+		var totalEnyclo = 0;
 
-	// Send the query and handle the response by logging the data
-	// to the console.
-	queryObj.setQuery(query);                                                                
-	queryObj.send(function(e) {
-    
-	var countEncylo = 0;
-	var totalEnyclo = 0;
+		var countArticles = 0;
+		var totalArticles = 0;
 
-	var countArticles = 0;
-	var totalArticles = 0;
+		var countCatalogs = 0;
+		var totalCatalogs = 0;
 
-	var countCatalogs = 0;
-	var totalCatalogs = 0;
+		var countClassMats = 0;
+		var totalClassMats = 0;
 
-	var countClassMats = 0;
-	var totalClassMats = 0;
+		var countGoogle = 0;
+		var totalGoogle = 0;
 
-	var countGoogle = 0;
-	var totalGoogle = 0;
+		var countWiki = 0;
+		var totalWiki = 0;
 
-	var countWiki = 0;
-	var totalWiki = 0;
+		ourData = e.getDataTable();
 
-	ourData = e.getDataTable();
+		
 
-	
+		aggTable = google.visualization.data.group(
+		  // input data
+		  ourData,
+		  // key columns (columns to group by)
+		  [0, 1],
+		  // third column (index 2) will be averaged and put in a column called "avg"
+		  [{'column': 2, 'aggregation': google.visualization.data.avg, 'type': 'number', 'label': 'avg'}]);
 
-	aggTable = google.visualization.data.group(
-	  // input data .getFilteredRows([{column: 0, value: selectedMajor}])
-	  ourData,
-	  // key columns (columns to group by)
-	  [0, 1],
-	  // third column (index 2) will be summed
-	  [{'column': 2, 'aggregation': google.visualization.data.avg, 'type': 'number', 'label': 'avg'}]);
+		//the following for-loop-switch-statement is used to replace the number
+		//representation of 'start' column with its corresponding string so that
+		//the x-axis is properly labelled when it is graphed.
+		var correspondingAverages = aggTable.getFilteredRows([{column: 0, value: selectedMajor}]);
+		for (var i = 0; i < correspondingAverages.length; i++) {
+			var startValue = parseInt(aggTable.getValue(correspondingAverages[i], 1));
+			
+			var s = "startValue at "+ i + " = " + startValue;
+			console.log(s);
 
-	console.log(aggTable);
-	
+			switch(startValue){
+		  	case 1:
+		  		aggTable.setCell(correspondingAverages[i], 1, 'Library-Encyclopedias');
+		  		console.log('renamed encylos');
+		  		break;
+		  	case 2:
+		  		aggTable.setCell(correspondingAverages[i], 1, 'Library-Articles');
+		  		break;
+		  	case 3:
+		  		aggTable.setCell(correspondingAverages[i], 1, 'Library-Catalogs');
+		  		break;
+		  	case 4:
+		  		aggTable.setCell(correspondingAverages[i], 1, 'Class Materials');
+		  		break;
+		  	case 5:
+		  		aggTable.setCell(correspondingAverages[i], 1, 'Web-Google');
+		  		break;
+		  	case 6:
+		  		aggTable.setCell(correspondingAverages[i], 1, 'Web-Wikipedia');
+		  		break;
+		  }
+		}
+	    
+	    //for now this will pull the data from the button.                                                           
+	    //var selectedMajor = document.getElementById('major').value;
+	                                   
+	    views[selectedMajor] = new google.visualization.DataView(aggTable);
 
-	var correspondingAverages = aggTable.getFilteredRows([{column: 0, value: selectedMajor}]);
-	for (var i = 0; i < correspondingAverages.length; i++) {
-		var startValue = parseInt(aggTable.getValue(correspondingAverages[i], 1));
-		switch(startValue){
-	  	case 1:
-	  		aggTable.setCell(correspondingAverages[i], 1, 'Library-Encyclopedias');
-	  		break;
-	  	case 2:
-	  		aggTable.setCell(correspondingAverages[i], 1, 'Library-Articles');
-	  		break;
-	  	case 3:
-	  		aggTable.setCell(correspondingAverages[i], 1, 'Library-Catalogs');
-	  		break;
-	  	case 4:
-	  		aggTable.setCell(correspondingAverages[i], 1, 'Class Materials');
-	  		break;
-	  	case 5:
-	  		aggTable.setCell(correspondingAverages[i], 1, 'Web-Google');
-	  		break;
-	  	case 6:
-	  		aggTable.setCell(correspondingAverages[i], 1, 'Web-Wikipedia');
-	  		break;
-	  }
-	}
-
-	// Log the raw response to the console.
-    console.log(aggTable.getValue(correspondingAverages[1], 1));
-    
-    //for now this will pull the data from the button.                                                           
-    //var selectedMajor = document.getElementById('major').value;
-                                   
-    views[selectedMajor] = new google.visualization.DataView(aggTable);
-
-	views[selectedMajor].setRows(views[selectedMajor].getFilteredRows([{column: 0, value: selectedMajor}]));
+		views[selectedMajor].setRows(views[selectedMajor].getFilteredRows([{column: 0, value: selectedMajor}]));
 
 
-    // Get a subset of the columns.                                                                            
-    views[selectedMajor].setColumns([1, 2]);
+	    // Get a subset of the columns.                                                                            
+	    views[selectedMajor].setColumns([1, 2]);
 
-    // Draw the chart for the initial academic year. //had a .toDataTable() part after ]                                                          
-    chart.draw(views[selectedMajor].toDataTable(), options);
+	    // Draw the chart for the initial academic year. //had a .toDataTable() part after ]                                                          
+	    chart.draw(views[selectedMajor].toDataTable(), options);
 	});
+
+}
+
+
+function vizInit() {
+	selectedMajor = majorSet.business;
+	getData();
+	//if need be, copy and paste getData() below 
+
 }
