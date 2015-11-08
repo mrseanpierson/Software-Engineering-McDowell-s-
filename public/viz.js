a97d0c3eda0be3a94510704ab51c0164f6279df5
Sean Pierson <mrseanpierson1@gmail.com>
2015-11-08 13:42:04 -0800

    used google.visualization.data.group to average confidence level rather than extracting data and doing it ourselves. Graph displays proper data, but in all numbers. TODO: x-axis label to strings

--- public/viz.js
+++ public/viz.js
@@ -13,15 +13,14 @@
 
 var librs = librs || {};
 
-google.load('visualization', '1', {packages: ['corechart']});
-
+google.load('visualization', '1', {packages: ['corechart', 'table']});
 //when the page is done loading, call 'drawChart'
 google.setOnLoadCallback(vizInit);
 //variables from step 5
-var data;
+var ourData;
 var views = {};
 var totals = {};
-
+var aggTable;
 var majorSet = {
 		business: 1,
 		humanities_history: 2,
@@ -33,47 +32,35 @@ var majorSet = {
 		education: 8
 	};
 
-	var locationStarted = {
-    Encyclopedias: 1,
-    Articles: 2,
-    Catalog: 3,
-    ClassMaterials: 4,
-    Google: 5,
-    Wikipedia: 6
-};
+	
 
 var selectedMajor = majorSet.business;
 var chart;
 var options = {
         width: 700,
         height: 400,
-	title: 'Confidence of students based on where research is started',
+		title: 'Confidence of students based on where research is started',
         hAxis: {
-            title: 'Research Start',
-            
+            title: 'Research Start'
         },
         vAxis: {
             title: 'Confidence'
         },
-	legend: { 
-	    position: 'none' 
-	},
-	animation: {
+		legend: { 
+	    	position: 'none' 
+		},
+		animation: {
             "startup" : true,
             "duration" : 500
-        },
-    bar: {
-    	groupWidth: 20
-    },
+        }
     };
     
 function vizController(selectedMajor) {
 
 	if(views[selectedMajor] === undefined){
-		views[selectedMajor] = new google.visualization.DataView(data);
-		console.log(data);
-		views[selectedMajor].setRows(views[selectedMajor].getFilteredRows([{column: 1, value: selectedMajor}]));
-		views[selectedMajor].setColumns(3);
+		views[selectedMajor] = new google.visualization.DataView(ourData);
+		views[selectedMajor].setRows(views[selectedMajor].getFilteredRows([{column: 0, value: selectedMajor}]));
+		views[selectedMajor].setColumns([1,3]);
 		chart.draw(views[selectedMajor].toDataTable(), options);
 	}
 }
@@ -83,7 +70,7 @@ function vizInit() {
 // Create a new viz object using the google API -- specifically,                                                                                                         
 // in the html file
 chart = new google.visualization.ColumnChart(document.getElementById('ex0'));
-
+chart.groupWidth = 100;
 //this gets the entire table
 var query = "SELECT major, start, confidence FROM 1fxvCbqTZgT21sArvYIp6zXBQzgCmVNUSwBZtu-BX"
 var opts = {sendMethod: 'auto'};
@@ -113,79 +100,59 @@ var queryObj = new google.visualization.Query('https://www.google.com/fusiontabl
 	var countWiki = 0;
 	var totalWiki = 0;
 
-	data = e.getDataTable();
-	var rowInds = data.getSortedRows([{column: 0, value: selectedMajor}]);
-	for (var i = 0; i < rowInds.length; i++) {
-	  var v = data.getValue(rowInds[i], 2);
-	  var s = data.getValue(rowInds[i], 1);
-	  switch(s){
+	ourData = e.getDataTable();
+
+	
+
+	aggTable = google.visualization.data.group(
+	  // input data .getFilteredRows([{column: 0, value: selectedMajor}])
+	  ourData,
+	  // key columns (columns to group by)
+	  [0, 1],
+	  // third column (index 2) will be summed
+	  [{'column': 2, 'aggregation': google.visualization.data.avg, 'type': 'number', 'label': 'avg'}]);
+
+	console.log(aggTable);
+	
+
+	var correspondingAverages = aggTable.getFilteredRows([{column: 0, value: selectedMajor}]);
+	for (var i = 0; i < correspondingAverages.length; i++) {
+		var startValue = aggTable.getValue(correspondingAverages[i], 1);
+		switch(startValue){
 	  	case 1:
-	  		data.setCell(rowInds[i], 1, 'Library-Encyclopedias');
-	  		countEncylo += 1;
-	  		totalEnyclo += v;
+	  		aggTable.setCell(correspondingAverages[i], 1, 'Library-Encyclopedias');
 	  		break;
 	  	case 2:
-	  		data.setCell(rowInds[i], 1, 'Library-Articles');
-	  		countArticles += 1;
-	  		totalArticles += v;
+	  		aggTable.setCell(correspondingAverages[i], 1, 'Library-Articles');
 	  		break;
 	  	case 3:
-	  		data.setCell(rowInds[i], 1, 'Library-Catalogs');
-	  		countCatalogs += 1;
-	  		totalCatalogs += v;
+	  		aggTable.setCell(correspondingAverages[i], 1, 'Library-Catalogs');
 	  		break;
 	  	case 4:
-	  		data.setCell(rowInds[i], 1, 'Class Materials');
-	  		countClassMats += 1;
-	  		totalClassMats += v;
+	  		aggTable.setCell(correspondingAverages[i], 1, 'Class Materials');
 	  		break;
 	  	case 5:
-	  		data.setCell(rowInds[i], 1, 'Web-Google');
-	  		countGoogle += 1;
-	  		totalGoogle += v;
+	  		aggTable.setCell(correspondingAverages[i], 1, 'Web-Google');
 	  		break;
 	  	case 6:
-	  		data.setCell(rowInds[i], 1, 'Web-Wiki');
-	  		countWiki += 1;
-	  		totalWiki += v;
+	  		aggTable.setCell(correspondingAverages[i], 1, 'Web-Wikipedia');
 	  		break;
 	  }
 	}
 
-	var averageEncylo = totalEnyclo/countEncylo;
-	var averageArticles = totalArticles/countArticles;
-	var averageCatalogs = totalCatalogs/countCatalogs;
-	var averageClassMats = totalClassMats/countClassMats;
-	var averageGoogle = totalGoogle/countGoogle;
-	var averageWiki = totalWiki/countWiki;
-
-	data.addColumn('number', 'averageEncylo');
-	data.addColumn('number', 'averageArticles');
-	data.addColumn('number', 'averageCatalogs');
-	data.addColumn('number', 'averageClassMats');
-	data.addColumn('number', 'averageGoogle');
-	data.addColumn('number', 'averageWiki');
-
-	data.setCell(0, 3, averageEncylo);
-	data.setCell(0, 4, averageArticles);
-	data.setCell(0, 5, averageCatalogs);
-	data.setCell(0, 6, averageClassMats);
-	data.setCell(0, 7, averageGoogle);
-	data.setCell(0, 8, averageWiki);
-
 	// Log the raw response to the console.
-    console.log(data);
+    //console.log(ourData);
     
     //for now this will pull the data from the button.                                                           
     //var selectedMajor = document.getElementById('major').value;
                                    
-    views[selectedMajor] = new google.visualization.DataView(data);
+    views[selectedMajor] = new google.visualization.DataView(aggTable);
 
 	views[selectedMajor].setRows(views[selectedMajor].getFilteredRows([{column: 0, value: selectedMajor}]));
 
 
     // Get a subset of the columns.                                                                            
-    views[selectedMajor].setColumns([1, 3, 4, 5, 6, 7, 8]);
+    views[selectedMajor].setColumns([1, 2]);
 
     // Draw the chart for the initial academic year. //had a .toDataTable() part after ]                                                          
     chart.draw(views[selectedMajor].toDataTable(), options);
