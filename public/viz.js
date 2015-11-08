9c921f3803f18e322727050a4691547b7f544ff5
Sean Pierson <mrseanpierson1@gmail.com>
2015-11-07 16:59:14 -0800

    Averaging of confidence based on major and start location done. Chart still displaying the wrong thing. Added key-value pairs for location start, added functionallity to manipulate the DataTable returned by the query.

--- public/viz.js
+++ public/viz.js
@@ -32,6 +32,16 @@ var majorSet = {
 		social_sciences: 7,
 		education: 8
 	};
+
+	var locationStarted = {
+    Encyclopedias: 1,
+    Articles: 2,
+    Catalog: 3,
+    ClassMaterials: 4,
+    Google: 5,
+    Wikipedia: 6
+};
+
 var selectedMajor = majorSet.business;
 var chart;
 var options = {
@@ -40,7 +50,7 @@ var options = {
 	title: 'Confidence of students based on where research is started',
         hAxis: {
             title: 'Research Start',
-            gridlines: {count: 12}
+            
         },
         vAxis: {
             title: 'Confidence'
@@ -52,6 +62,9 @@ var options = {
             "startup" : true,
             "duration" : 500
         },
+    bar: {
+    	groupWidth: 20
+    },
     };
     
 function vizController(selectedMajor) {
@@ -59,8 +72,8 @@ function vizController(selectedMajor) {
 	if(views[selectedMajor] === undefined){
 		views[selectedMajor] = new google.visualization.DataView(data);
 		console.log(data);
-		views[selectedMajor].setRows(views[selectedMajor].getFilteredRows([{column: 2, value: selectedMajor}]));
-		views[selectedMajor].setColumns([0, 3]);
+		views[selectedMajor].setRows(views[selectedMajor].getFilteredRows([{column: 1, value: selectedMajor}]));
+		views[selectedMajor].setColumns(3);
 		chart.draw(views[selectedMajor].toDataTable(), options);
 	}
 }
@@ -81,33 +94,100 @@ var queryObj = new google.visualization.Query('https://www.google.com/fusiontabl
 	// to the console.
 	queryObj.setQuery(query);                                                                
 	queryObj.send(function(e) {
-       
+    
+	var countEncylo = 0;
+	var totalEnyclo = 0;
+
+	var countArticles = 0;
+	var totalArticles = 0;
+
+	var countCatalogs = 0;
+	var totalCatalogs = 0;
+
+	var countClassMats = 0;
+	var totalClassMats = 0;
+
+	var countGoogle = 0;
+	var totalGoogle = 0;
+
+	var countWiki = 0;
+	var totalWiki = 0;
+
 	data = e.getDataTable();
+	var rowInds = data.getSortedRows([{column: 0, value: selectedMajor}]);
+	for (var i = 0; i < rowInds.length; i++) {
+	  var v = data.getValue(rowInds[i], 2);
+	  var s = data.getValue(rowInds[i], 1);
+	  switch(s){
+	  	case 1:
+	  		data.setCell(rowInds[i], 1, 'Library-Encyclopedias');
+	  		countEncylo += 1;
+	  		totalEnyclo += v;
+	  		break;
+	  	case 2:
+	  		data.setCell(rowInds[i], 1, 'Library-Articles');
+	  		countArticles += 1;
+	  		totalArticles += v;
+	  		break;
+	  	case 3:
+	  		data.setCell(rowInds[i], 1, 'Library-Catalogs');
+	  		countCatalogs += 1;
+	  		totalCatalogs += v;
+	  		break;
+	  	case 4:
+	  		data.setCell(rowInds[i], 1, 'Class Materials');
+	  		countClassMats += 1;
+	  		totalClassMats += v;
+	  		break;
+	  	case 5:
+	  		data.setCell(rowInds[i], 1, 'Web-Google');
+	  		countGoogle += 1;
+	  		totalGoogle += v;
+	  		break;
+	  	case 6:
+	  		data.setCell(rowInds[i], 1, 'Web-Wiki');
+	  		countWiki += 1;
+	  		totalWiki += v;
+	  		break;
+	  }
+	}
 
+	var averageEncylo = totalEnyclo/countEncylo;
+	var averageArticles = totalArticles/countArticles;
+	var averageCatalogs = totalCatalogs/countCatalogs;
+	var averageClassMats = totalClassMats/countClassMats;
+	var averageGoogle = totalGoogle/countGoogle;
+	var averageWiki = totalWiki/countWiki;
 
-	
+	data.addColumn('number', 'averageEncylo');
+	data.addColumn('number', 'averageArticles');
+	data.addColumn('number', 'averageCatalogs');
+	data.addColumn('number', 'averageClassMats');
+	data.addColumn('number', 'averageGoogle');
+	data.addColumn('number', 'averageWiki');
+
+	data.setCell(0, 3, averageEncylo);
+	data.setCell(0, 4, averageArticles);
+	data.setCell(0, 5, averageCatalogs);
+	data.setCell(0, 6, averageClassMats);
+	data.setCell(0, 7, averageGoogle);
+	data.setCell(0, 8, averageWiki);
+
 	// Log the raw response to the console.
     console.log(data);
     
     //for now this will pull the data from the button.                                                           
     //var selectedMajor = document.getElementById('major').value;
-
-    // Next, create the object and get the rows 
-	// corresponding to "selectedMajor".                                   
+                                   
     views[selectedMajor] = new google.visualization.DataView(data);
-   	//What i believe we should do is the following:
-   	//1. get all rows of selectedMajor 
-   	//2. get all rows for each start location from 1.'s 
-    //3. add up all the values from 2.'s new DataView while counting how many there are
-    //	 then divide the sum by the count number
-    //somthing along the lines of this, but for each one
-    //im unsure of if we have to do a new query for each of these.
-	var rowsOfSelectedMajorEncyclo = views[selectedMajor].setRows(views[selectedMajor].getFilteredRows([{column: 0, value: selectedMajor}, {column: 1, value: 1}]));
+
+	views[selectedMajor].setRows(views[selectedMajor].getFilteredRows([{column: 0, value: selectedMajor}]));
+
 
     // Get a subset of the columns.                                                                            
-    views[selectedMajor].setColumns([1]);
+    views[selectedMajor].setColumns([1, 3, 4, 5, 6, 7, 8]);
 
-    // Draw the chart for the initial academic year.                                                           
+    // Draw the chart for the initial academic year. //had a .toDataTable() part after ]                                                          
     chart.draw(views[selectedMajor].toDataTable(), options);
 	});
 }
